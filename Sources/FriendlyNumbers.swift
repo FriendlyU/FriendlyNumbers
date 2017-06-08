d7324ea20a9096c904b9b51f38867aef7fe60f82
Henry Saniuk <henry@henrysaniuk.com>
2017-06-08 16:52:51 -0400

    Better handle numbers less than 1000 and numbers that are multiples of 1000

--- Sources/FriendlyNumbers.swift
+++ Sources/FriendlyNumbers.swift
@@ -9,12 +9,24 @@
 
 import Foundation
 
+/// MARK: Int extensions
+
 extension Int {
-    public var friendlyNumber: String {
-        return "\(self.friendlyDigits)\(self.placevalueLetter ?? "")"
+    public var simpleFriendlyNumber: String {
+        return "\(self.simpleFriendlyDigits)\(self.placevalueLetter ?? "")"
     }
     
-    public var friendlyDigits: Int {
+    public var friendlyNumber: String {
+        let number: String
+        if self < 1000 || self % 1000 == 0 {
+            number = String(Int(self.friendlyDigits))
+        } else {
+            number = String(self.friendlyDigits)
+        }
+        return "\(number)\(self.placevalueLetter ?? "")"
+    }
+    
+    public var simpleFriendlyDigits: Int {
         if self < 1000000 {
             if self / 1000 == 0 {
                 return self % 1000
@@ -30,12 +42,32 @@ extension Int {
         }
     }
     
+    public var friendlyDigits: Double {
+        let divisor: Double
+        if self < 1000 {
+            divisor = 1.0
+        } else if self < 100000 {
+            divisor = 1000.0
+        } else {
+            divisor = 1000000.0
+        }
+        return (Double(self) / divisor).roundTo(places: 1)
+    }
+    
     public var placevalueLetter: String? {
         switch(self / 1000) {
         case 1..<1000: return "K"
         case 1000..<100000: return "M"
         default: return nil
-            
         }
+    }
+}
+
+/// MARK: Double extensions
+
+extension Double {
+    func roundTo(places: Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
     }
 }
